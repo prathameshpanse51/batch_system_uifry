dc0c757e3f673208c50a8f529d08e87f17fde715
prathameshpanse51 <93512551+prathameshpanse51@users.noreply.github.com>
2024-06-03 17:02:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body className={cn("min-h-screen bg-background", myFont.className)}>
         <ThemeProvider
           attribute="class"
